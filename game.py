c6d226878056003168c0c27752fd9c5c4fd635fb
Beamer94 <bmwilliams969@gmail.com>
2017-07-26 15:51:44 -0400

    Advanced Pad Collision added in Debug
    I've got the angular pad collision set to run in Debug mode. 
    It's really glitchy right now, but works roughly as expected. 
    Glitches include velocity rounding errors and Pad Hitbox errors.

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 #WIDTH = 1280
 #HEIGHT = 800
-
+from __future__ import print_function
 import random
 import pygame, sys
 import math
@@ -16,7 +16,7 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLACK = (0,0,0)
 SILVER = (192, 192, 192)
-BLUE = (66, 134, 244)
+BLUE = (0, 0, 255)
 YELLOW = (237, 249, 57)
 PURPLE = (189, 26, 221)
 
@@ -81,8 +81,10 @@ def run(window):
 def ball_init():
 	global balls
 	balls = []
-	init_vel = [-WIDTH/100, -HEIGHT/250]
-	#init_vel = [7, 0]
+	#init_vel = [-WIDTH/100, -HEIGHT/250]
+	init_vel = [0, 0]
+	if len(balls) > 0:
+		balls.pop()
 	balls.append(bb_objs.ball(WIDTH/2, HEIGHT/2, init_vel, PAD_WIDTH/15, SILVER))
 	#balls.append(bb_objs.ball(WIDTH/4,  HEIGHT-HEIGHT/15, init_vel, PAD_WIDTH/15, SILVER))	#Test Ball
 	
@@ -96,7 +98,7 @@ def keydown(event):
 		pad.vel += WIDTH/(WIDTH/6)
 		
 	if debug == True:	
-		print(pad.vel)
+		print("Pad Velocity:",pad.vel)
 
 
 def keyup(event):
@@ -104,10 +106,20 @@ def keyup(event):
 
 	if event.key == K_LEFT:
 		pad.vel += WIDTH/(WIDTH/6)
+		if balls[0].vel[0] == 0 and balls[0].vel[1] == 0:
+			balls[0].vel[1] = HEIGHT/200
+
 	if event.key == K_RIGHT:
 		pad.vel += -WIDTH/(WIDTH/6)
-		
-	if debug == True:	
+		if balls[0].vel[0] == 0 and balls[0].vel[1] == 0:
+			balls[0].vel[1] = HEIGHT/200
+	
+	if event.key == K_SPACE:
+		if balls[0].vel[0] == 0 and balls[0].vel[1] == 0:
+			balls[0].vel[1] = HEIGHT/200
+		None
+
+	if debug == True:
 		print(pad.vel)
 
 
@@ -135,13 +147,14 @@ def draw(canvas):
 	elif pad.pos[0] >= WIDTH - PAD_WIDTH/2 - WIDTH/20 and pad.vel < 0:	#Right Bound
 		pad.pos[0] += pad.vel
 
-	# Draws Ball
+
+	# Draws Balls
 	pygame.draw.circle(canvas, SILVER, balls[0].pos, balls[0].radius, 0)
 
 	i = 0	#replace with for loop, that iterates all over balls list
 	# Update Ball
-	balls[i].pos[0] += balls[i].vel[0]
-	balls[i].pos[1] += balls[i].vel[1]
+	balls[i].pos[0] += int(balls[i].vel[0])
+	balls[i].pos[1] += int(balls[i].vel[1])
 
 	# Wall Collision 
 	if balls[i].pos[0] <= WIDTH/20 + balls[i].radius:	# left
@@ -153,7 +166,26 @@ def draw(canvas):
 
 	# Pad Collision
 	if (balls[i].pos[1] >= pad.pos[1] - PAD_HEIGHT/2 and balls[i].pos[1] <= pad.pos[1] + PAD_HEIGHT/2) and (balls[i].pos[0] >= pad.pos[0] - PAD_WIDTH/2 and balls[i].pos[0] <= pad.pos[0] + PAD_WIDTH/2):	# Hits Top of Pad
-		balls[i].vel[1] = -balls[i].vel[1]
+		if debug == True:
+			vel_vect = (balls[i].vel[0]**2 + balls[i].vel[1]**2)**(0.5)
+			contact_frac = (float(balls[i].pos[0]) - float(pad.pos[0]))/(PAD_WIDTH/2)
+
+			if contact_frac < 0:
+				balls[i].vel[0] = math.ceil(math.sin(math.pi * contact_frac) * vel_vect * 1)
+				balls[i].vel[1] = math.ceil(math.cos(math.pi * contact_frac) * vel_vect * 1)
+			elif contact_frac > 0:
+				balls[i].vel[0] = math.floor(math.sin(math.pi * contact_frac) * vel_vect * 1)
+				balls[i].vel[1] = math.floor(math.cos(math.pi * contact_frac) * vel_vect * 1)
+			elif contact_frac == 0:
+				balls[i].vel[1] = -balls[i].vel[1]
+			
+
+			print(balls[i].vel[0], balls[i].vel[1], vel_vect)
+			#print('X:', math.sin(math.pi * contact_frac))
+			#print('Y:', math.cos(math.pi * contact_frac))
+		else: 
+			balls[i].vel[1] = -balls[i].vel[1]
+
 	elif (balls[i].pos[1] >= pad.pos[1] - PAD_HEIGHT/2 and balls[i].pos[1] <= pad.pos[1] + PAD_HEIGHT/2) and (balls[i].pos[0] >= pad.pos[0] - PAD_WIDTH/2 - math.fabs(balls[i].vel[0])*3 + pad.vel and balls[i].pos[0] <= pad.pos[0] - PAD_WIDTH/2):	# Hits Side of Pad Left
 		balls[i].vel[0] = -balls[i].vel[0]
 		balls[i].vel[1] = 5
@@ -164,13 +196,16 @@ def draw(canvas):
 		balls[i].vel[1] = 5
 		if debug == True:
 			print("hit right")
-
-	
 	
 	# Ball Death
-	if balls[i].pos[1] == HEIGHT: #pad.pos[1] - PAD_HEIGHT:	# Misses Pad
-		print("DEAD")
+	if balls[i].pos[1] >= HEIGHT: #pad.pos[1] - PAD_HEIGHT:	# Misses Pad
+		ball_init()
+		pad.pos = [WIDTH/2, HEIGHT-HEIGHT/15]
+		if debug == True:
+			print("DEAD")
 		
+
+
 
 		# Hit box displays
 	if debug == True:
@@ -185,11 +220,10 @@ def draw(canvas):
 		pygame.draw.line(canvas, BLUE, 	[pad.pos[0] + PAD_WIDTH/2 + math.fabs(balls[i].vel[0])*3,0], [pad.pos[0] + PAD_WIDTH/2 + math.fabs(balls[i].vel[0])*3,HEIGHT] , 1) # Right
 
 		# Ball
-		pygame.draw.line(canvas, WHITE, [balls[i].pos[0] - balls[i].radius, 0], [balls[i].pos[0] - balls[i].radius, HEIGHT] , 1) # Left
-		pygame.draw.line(canvas, WHITE, [balls[i].pos[0] + balls[i].radius, 0], [balls[i].pos[0] + balls[i].radius, HEIGHT] , 1) # Right
-		pygame.draw.line(canvas, WHITE, [0, balls[i].pos[1] - balls[i].radius], [WIDTH, balls[i].pos[1] - balls[i].radius] , 1) # Top
-		pygame.draw.line(canvas, WHITE, [0, balls[i].pos[1] + balls[i].radius], [WIDTH, balls[i].pos[1] + balls[i].radius] , 1) # Bottom
-	
+		pygame.draw.line(canvas, WHITE, [balls[i].pos[0], 0], [balls[i].pos[0], HEIGHT] , 1) # Vertical
+		pygame.draw.line(canvas, WHITE, [0, balls[i].pos[1]], [WIDTH, balls[i].pos[1]] , 1) # Horizontal
+
+
 
 
 if __name__ == "__main__":
